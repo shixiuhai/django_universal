cecf347fa91b5376fb39c7cf0f9cf6736d52f05a
shixiuhai <15256728901@163.com>
2022-11-13 11:31:38 +0800

    更新了部分django drf配置

---
+++ qqOauth/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class QqoauthConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'qqOauth'
---
+++ wechatOauth/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WechatoauthConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'wechatOauth'
